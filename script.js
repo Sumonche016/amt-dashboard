ec9c83418ee3cc5de51e412954a900c9ce947eba
Sumon Bala <Sumonche016@gmail.com>
2023-12-13 03:51:37 +0600

    media screen added

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const dashboardBtn = document.getElementById("dashboardBtn");
   dashboardBtn.classList.add("active-nav-btn");
 });
+
+// for toggle mobile menu
+const navButtons = document.getElementById("side-nav");
+const hamburger = document.getElementById("hamburger-icon");
+const closeNav = document.getElementById("close-nav");
+
+hamburger.addEventListener("click", () => {
+  navButtons.classList.add("show-sidenav");
+});
+
+closeNav.addEventListener("click", () => {
+  navButtons.classList.remove("show-sidenav");
+});
